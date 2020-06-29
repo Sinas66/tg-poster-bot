336666e9892d8ea3d27ff42c2d09d3ba476c1cee
Pasha <Pashkomania@gmail.com>
2020-06-29 18:31:00 +0300

    NODE_ENV

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const publicId =
-  process.env.NODE === "production"
+  process.env.NODE_ENV === "production"
     ? process.env.PROD_PUBLIC_ID || 0
     : process.env.DEV_PUBLIC_ID || 0;
+
+console.log("publicId", publicId);
 
 module.exports = {
   port: process.env.PORT || 3000,
